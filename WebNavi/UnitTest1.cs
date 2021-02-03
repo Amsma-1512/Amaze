ddc1ebe95423f2a94d3cf09e03c2ec81338d4225
Amsma <amsma.a@gmail.com>
2021-02-04 00:48:46 +0800

    Amazon product search testcase

--- WebNavi/UnitTest1.cs
+++ WebNavi/UnitTest1.cs
@@ -5,6 +5,7 @@ using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
 using Microsoft.Ajax.Utilities;
 
+
 namespace WebNavi
 {
     [TestClass]
@@ -17,9 +18,7 @@ namespace WebNavi
         [AssemblyInitialize]
         public static void Setup(TestContext context)
         {
-            ChromeD = new ChromeDriver(@"C:\Users\amsma\source\repos\Amaze\packages\Selenium.WebDriver.ChromeDriver.88.0.4324.9600\driver\win32");
-           
-            
+            ChromeD = new ChromeDriver(@"C:\Users\amsma\source\repos\Amaze\packages\Selenium.WebDriver.ChromeDriver.88.0.4324.9600\driver\win32");  
         }
 
         [TestMethod]
@@ -31,20 +30,18 @@ namespace WebNavi
             //Search for Laptop
             ChromeD.FindElement(By.Id("twotabsearchtextbox")).SendKeys("Laptop");
             ChromeD.FindElement(By.Id("nav-search-submit-button")).SendKeys(Keys.Enter);
-            ChromeD.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+            //ChromeD.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
             //To Select the First result on searching Laptop, also to skip the AdHolders
             //Selects the first actual result ignoring the advertisements and Sponsors
             ChromeD.FindElement(By.XPath("(//a[@class='a-link-normal a-text-normal' and ancestor::div[@class='s-result-item s-asin sg-col-0-of-12 sg-col-16-of-20 sg-col sg-col-12-of-16']])[1]")).Click();
-            ChromeD.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+            ChromeD.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);          
             string Price = ChromeD.FindElement(By.Id("price_inside_buybox")).Text;
             double priceValue = Convert.ToDouble(Price.Substring(1));
-            //if (priceValue > basePrice) { }
-            //   Console.WriteLine(priceValue);
-            //To Check if the Laptop price is greater than 100$
+            //To assert if the Laptop price is greater than 100$
+            //Test should pass if the price is greater than 100$
+            //Test should fail if price is less than or equal to 100$
             Assert.IsTrue(priceValue > basePrice, "Price is less than $100");
-            Console.WriteLine ("Execution completed");
-            ChromeD.Close();
-            ChromeD.Quit();
+            Console.WriteLine("Execution completed");
         }
 
         
